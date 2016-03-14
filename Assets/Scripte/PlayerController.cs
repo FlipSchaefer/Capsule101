fc57c04ca95dfa92ac0435a66e942468d5e4c35c
FlipSchaefer <mr.sing@gmx.net>
2016-03-14 21:46:42 +0100

    Add shooting
    add shooting
    *Bug--> client -no force on bullet

--- Assets/Scripte/PlayerController.cs
+++ Assets/Scripte/PlayerController.cs
@@ -3,6 +3,10 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
     void Update()
     {
         if (!isLocalPlayer)
@@ -14,5 +18,29 @@ public class PlayerController : NetworkBehaviour
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
     }
+
+    [Command]
+    void CmdFire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
+        Rigidbody rigidBody = bullet.GetComponent<Rigidbody>();
+        rigidBody.AddForce(bullet.transform.forward * 6, ForceMode.VelocityChange);
+
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet.gameObject, 5);
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
 }
